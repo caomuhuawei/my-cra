82c51e7c59c35e2cb35a44add9c89658dca97e5c
david.c <david.c@tc-gaming.com>
2020-09-17 17:59:30 +0800

    feat: 优化eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,38 +2,21 @@ module.exports = {
   globals: {
     window: true,
     document: true,
-    lottTranslator: true,
-    FormData: true,
-    FileReader: true,
-    CryptoJS: true,
     location: true,
     history: true,
-    reRsa: true,
   },
-  parser: "babel-eslint",
+  plugins: ["prettier"],
   parserOptions: {
+    parser: "babel-eslint",
     ecmaVersion: 2016,
     sourceType: "module",
     ecmaFeatures: {
       jsx: true,
     },
   },
-  plugins: ["prettier"],
   extends: ["react-app", "plugin:prettier/recommended", "prettier/react"],
-  settings: {
-    react: {
-      version: "15.5.4",
-    },
-  },
   rules: {
     "max-len": [1, 120, 2, { ignoreComments: true }],
     "prettier/prettier": "error",
-    eqeqeq: [2, "smart"],
-    "import/no-webpack-loader-syntax": ["off"],
-    "no-restricted-globals": 0,
-    "no-unused-vars": ["off"],
-    "jsx-a11y/anchor-is-valid": "off",
-    "react/no-deprecated": 0,
-    "no-unused-expressions": ["off"],
   },
 };
